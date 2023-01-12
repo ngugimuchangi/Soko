b673edfdbb2b10ffdd2fd1f22b999a65c14eab4c
ngugimuchangi <tech.curious14@gmail.com>
2023-01-12 08:24:04 +0300

    removed constraint check due to no support by older mysql versions

--- models/order.py
+++ models/order.py
@@ -4,8 +4,7 @@
         and methods
 """
 from models.base_model import Base, BaseModel
-from sqlalchemy import (Column, CheckConstraint,
-                        Float, String, Text)
+from sqlalchemy import Column, Float, String, Text
 from sqlalchemy.orm import relationship
 
 
@@ -18,7 +17,7 @@ class Order(BaseModel, Base):
             vat: (int): vat charged
             shipping_cost(int): shipping cost
             shipping_address(str): shipping address
-            order_status(int): order completeion status (options:
+            order_status(int): order completion status (options:
                                 awaiting payment, paid, completed)
             order_details: relationship with order detail's table
             transactions: relationship with transaction's table
@@ -26,14 +25,9 @@ class Order(BaseModel, Base):
     __tablename__ = "orders"
 
     customer_id = Column(String(60), nullable=True)
-    subtotal = Column(Float, CheckConstraint('subtotal > 0',
-                      name='subtotal_positive'),
-                      nullable=False)
-    vat = Column(Float, CheckConstraint('vat > 0',
-                 name='vat_positive'),
-                 nullable=False)
-    shipping_cost = Column(Float, CheckConstraint('shipping_cost > 0.00',
-                           name='shipping_cost_positive'), nullable=False)
+    subtotal = Column(Float, nullable=False)
+    vat = Column(Float,nullable=False)
+    shipping_cost = Column(Float, nullable=False)
     shipping_address = Column(Text, nullable=True)
     order_status = Column(String(60), default="awating payment",
                           nullable=False)
