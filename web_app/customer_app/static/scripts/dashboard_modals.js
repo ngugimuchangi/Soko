5a689a8b1ead7168c27ffc4ced08548fc9d7c159
ngugimuchangi <tech.curious14@gmail.com>
2023-01-22 11:55:50 +0300

    added card detail modal

--- web_app/customer_app/static/scripts/dashboard_modals.js
+++ web_app/customer_app/static/scripts/dashboard_modals.js
@@ -2,6 +2,8 @@
 // 1. Updating dashboard modal content
 // 2. Updating user details
 // 3. Posting product reviews
+// NB Will change editing and adding new data to use
+// ajax calls to backend API latter
 $(document).ready(function () {
   // START OF COMMON MODAL FEATURES
   //  Modal dimensions adjustments
@@ -48,15 +50,14 @@ $(document).ready(function () {
       "submit-new-address-details",
       "submit-new-card-details",
     ]);
+    $(".modal-container").removeClass("show-modal");
   }
   $(".close-modal-x").click(function (event) {
     event.stopPropagation();
-    $(".modal-container").toggleClass("show-modal");
     removeModalDetails();
   });
 
   $(".modal-container").click(function () {
-    $(".modal-container").removeClass("show-modal");
     removeModalDetails();
   });
   $(".modal").click(function (event) {
@@ -141,7 +142,6 @@ $(document).ready(function () {
     $("#user-email span").text(email);
     $("#user-phone-number span").text(phoneNumber);
     removeModalDetails();
-    $(".modal-container").removeClass("show-modal");
   });
   // END OF MY PROFILE MODAL
 
@@ -203,6 +203,8 @@ $(document).ready(function () {
       "Add"
     );
   });
+
+  // Editing an existing address
   $(document).on("click", ".edit-address", function () {
     currentAddressId = $(this).parent().parent().attr("data-id");
     const addressHolder = $(this)
@@ -211,10 +213,10 @@ $(document).ready(function () {
       .children("p");
     const firstName = addressHolder.attr("f-name");
     const lastName = addressHolder.attr("l-name");
-    const addressDetails = $(this).parent().siblings("#address-details").text();
+    const addressDetails = $(this).parent().siblings(".address-details").text();
     const phoneNumber = $(this)
       .parent()
-      .siblings("#address-phone-number")
+      .siblings(".address-phone-number")
       .text();
     const modalContent = `<label for="first-name" class="modal-label">First Name</label>
       <input
@@ -274,6 +276,7 @@ $(document).ready(function () {
       "Update"
     );
   });
+
   // Submit address details info
   $(".modal-content").on("click", ".update-address-details", function (event) {
     event.stopPropagation();
@@ -286,40 +289,22 @@ $(document).ready(function () {
     const currentAddress = $(`.address[data-id |= "${currentAddressId}"]`);
     currentAddress
       .children()
-      .children("#address-holder")
+      .children(".address-holder-names")
       .attr("f-name", firstName);
     currentAddress
       .children()
-      .children("#address-holder")
+      .children(".address-holder-names")
       .attr("l-name", lastName);
     currentAddress
       .children()
-      .children("#address-holder")
+      .children(".address-holder-names")
       .text(`${firstName} ${lastName}`);
-    currentAddress.children("#address-phone-number").text(phoneNumber);
-    currentAddress.children("#address-details").text(addressDetails);
+    currentAddress.children(".address-phone-number").text(phoneNumber);
+    currentAddress.children(".address-details").text(addressDetails);
     if ($("input#set-default-address").is(":checked")) {
-      currentAddress
-        .siblings(".address")
-        .children(".address-holder")
-        .children(".fa-star")
-        .removeClass(["default", "fa-solid"]);
-      currentAddress
-        .siblings(".address")
-        .children(".address-holder")
-        .children(".fa-star")
-        .addClass("fa-regular");
-      currentAddress
-        .children(".address-holder")
-        .children(".fa-star")
-        .removeClass("fa-regular");
-      currentAddress
-        .children(".address-holder")
-        .children(".fa-star")
-        .addClass(["fa-solid", "default"]);
+      setDefault(currentAddress, ".address", ".address-holder");
     }
     removeModalDetails();
-    $(".modal-container").removeClass("show-modal");
   });
 
   // Submit new address details
@@ -344,13 +329,13 @@ $(document).ready(function () {
           Math.random() * 1000
         )}">
             <div class="address-holder flex-row">
-              <p id="address-holder" f-name="${firstName}" l-name="${lastName}">
+              <p class="address-holder-names" f-name="${firstName}" l-name="${lastName}">
                 ${firstName} ${lastName}
               </p>
               <i class="fa-regular fa-star dashboard-content-icon"></i>
             </div>
-            <p id="address-phone-number">${phoneNumber}</p>
-            <p id="address-details">${addressDetails}</p>
+            <p class="address-phone-number">${phoneNumber}</p>
+            <p class="address-details">${addressDetails}</p>
             <div class="delete-edit flex-row">
               <i
                 class="fa-solid fa-trash-can dashboard-content-icon delete-address"
@@ -363,31 +348,233 @@ $(document).ready(function () {
 
       $(".user-addresses-div").prepend(currentAddress);
       if ($("input#set-default-address").is(":checked")) {
-        currentAddress
-          .siblings(".address")
-          .children(".address-holder")
-          .children(".fa-star")
-          .removeClass(["default", "fa-solid"]);
-        currentAddress
-          .siblings(".address")
-          .children(".address-holder")
-          .children(".fa-star")
-          .addClass("fa-regular");
-        currentAddress
-          .children(".address-holder")
-          .children(".fa-star")
-          .removeClass("fa-regular");
-        currentAddress
-          .children(".address-holder")
-          .children(".fa-star")
-          .addClass(["fa-solid", "default"]);
+        setDefault(currentAddress, ".address", ".address-holder");
       }
       removeModalDetails();
-      $(".modal-container").removeClass("show-modal");
     }
   );
+
+  //  Function to set address or card to default
+  function setDefault(currentItem, currentItemClass, defaultSection) {
+    currentItem
+      .siblings(currentItemClass)
+      .children(defaultSection)
+      .children(".fa-star")
+      .removeClass(["default", "fa-solid"])
+      .addClass("fa-regular");
+    currentItem
+      .children(defaultSection)
+      .children(".fa-star")
+      .removeClass("fa-regular")
+      .addClass(["fa-solid", "default"]);
+  }
   // END OF ADDRESS MODULE
 
   // START OF CARD DETAILS
+  let currentCardId;
+
+  // Editing an existing card
+  $(document).on("click", ".edit-card", function () {
+    currentCardId = $(this).parent().parent().attr("data-id");
+    const cardNumber = $(this)
+      .parent()
+      .siblings(".card-number")
+      .attr("data-name");
+    const cardHolder = $(this)
+      .parent()
+      .siblings(".card-holder")
+      .children(".card-holder-names")
+      .text();
+    const expiryDate = $(this)
+      .parent()
+      .siblings(".expiry-date")
+      .attr("expiry-date");
+    modalContent = ` <label for="card-number" class="modal-label">Card Number</label>
+    <input
+        type="text"
+        name="card-number"
+        id="card-number"
+        class="modal-input"
+        value= "${cardNumber}"
+        required
+        oninvalid="this.setCustomValidity('Enter the card number')"
+      />
+      <label for="card-holder" class="modal-label">Card Holder's Name</label>
+      <input
+        type="text"
+        name="last-name"
+        id="last-name"
+        class="modal-input"
+        value= "${cardHolder}"
+        required
+        oninvalid="this.setCustomValidity('Enter card holder's names')"
+      />
+      <label for="expiry-date" class="modal-label">Expiry date</label>
+      <input
+        type="text"
+        name="expiry-date"
+        id="expiry-date"
+        class="modal-input"
+        value="${expiryDate}"
+        max-length="5"
+        min-length="5"
+        placeholder="MM/YY"
+        required
+        oninvalid="this.setCustomValidity('Enter cards expiry date')"
+      />
+      <label for="cvv" class="modal-label">CVV</label>
+      <input
+        type="text"
+        name="cvv"
+        id="cvv"
+        max-length="3"
+        min-length="3"
+        class="modal-input"
+        required
+        oninvalid="this.setCustomValidity('Enter correct card verification value')"
+      />
+      <div class="set-default">
+        <input
+        type="checkbox"
+        name="set-default-card"
+        id="set-default-card"
+        class="modal-input"
+      />
+        <label for="set-default-card" class="modal-label">
+        Set as default payment card </label>
+      </div>
+      `;
+    updateModal("Edit Card", modalContent, "update-card-details", "Update");
+  });
+
+  // Submit updated card details
+  $(".modal-content").on("click", ".update-card-details", function (event) {
+    event.stopPropagation();
+    const currentCard = $(`.card[data-id |= "${currentCardId}"]`);
+    const cardNumber = $("input#card-number").val();
+    const lastDigits = cardNumber.split("-");
+    const cardHolder = $("input#card-names").val();
+    const expiryDate = $("input#expiry-date").val();
+    const cvv = $("input#cvv").val();
+    const fields = new Array(cardNumber, cardHolder, expiryDate, cvv);
+    if (fields.some((field) => field == "")) return;
+    currentCard
+      .children(".card-holder")
+      .children(".card-holder-names")
+      .text(cardHolder);
+    currentCard
+      .children(".card-number")
+      .text(`XXXX-XXXX-XXXX-${lastDigits[lastDigits.length - 1]}`);
+    currentCard.children(".card-number").attr("data-name", cardNumber);
+    currentCard.children(".expiry-date").text(`Expires on: ${expiryDate}`);
+    currentCard.children(".expiry-date").attr("data-name", cardNumber);
+    if ($("input#set-default-card").is(":checked")) {
+      setDefault(currentCard, ".card", ".card-holder");
+    }
+    removeModalDetails();
+  });
+
+  // Adding a new card
+  $(".add-card").click(function () {
+    modalContent = ` <label for="card-number" class="modal-label">Card Number</label>
+    <input
+        type="text"
+        name="card-number"
+        id="card-number"
+        class="modal-input"
+        required
+        oninvalid="this.setCustomValidity('Enter the card number')"
+      />
+      <label for="card-holder" class="modal-label">Card Holder's Name</label>
+      <input
+        type="text"
+        name="card-holder"
+        id="card-holder"
+        class="modal-input"
+        required
+        oninvalid="this.setCustomValidity('Enter card holder's names')"
+      />
+      <label for="expiry-date" class="modal-label">Expiry date</label>
+      <input
+        type="text"
+        name="expiry-date"
+        id="expiry-date"
+        class="modal-input"
+        max-length="5"
+        min-length="5"
+        placeholder="MM/YY"
+        required
+        oninvalid="this.setCustomValidity('Enter cards expiry date')"
+      />
+      <label for="cvv" class="modal-label">CVV</label>
+      <input
+        type="text"
+        name="cvv"
+        id="cvv"
+        max-length="3"
+        min-length="3"
+        class="modal-input"
+        required
+        oninvalid="this.setCustomValidity('Enter correct card verification value')"
+      />
+      <div class="set-default">
+        <input
+        type="checkbox"
+        name="set-default-card"
+        id="set-default-card"
+        class="modal-input"
+      />
+        <label for="set-default-card" class="modal-label">
+        Set as default payment card </label>
+      </div>
+      `;
+    updateModal("Add New Card", modalContent, "submit-new-card-details", "Add");
+  });
+  // Submit new card details
+  $(".modal-content").on("click", ".submit-new-card-details", function (event) {
+    event.stopPropagation();
+    const cardNumber = $("input#card-number").val();
+    const lastDigits = cardNumber.split("-");
+    const cardHolder = $("input#card-holder").val();
+    const expiryDate = $("input#expiry-date").val();
+    const fields = new Array(cardNumber, cardHolder, expiryDate, cvv);
+    if (fields.some((field) => field == "")) return;
+    const newCard = $(`
+          <article class="card user-item flex-column" data-id="${Math.floor(
+            Math.random() * 1000
+          )}">
+            <div class="card-holder flex-row">
+              <p class="card-holder-names">${cardHolder}</p>
+              <i class="fa-regular fa-star dashboard-content-icon"></i>
+            </div>
+            <p class="card-number" data-name="${cardNumber}">
+              XXXX-XXXX-XXXX-${lastDigits[lastDigits.length - 1]}
+            </p>
+            <p class="expiry-date" expiry-date="${expiryDate}">Expires on: ${expiryDate}</p>
+            <div class="delete-edit flex-row">
+              <i
+                class="fa-solid fa-trash-can dashboard-content-icon delete-card"
+              ></i>
+              <i
+                class="fa-solid fa-pen dashboard-content-icon edit-card edit"
+              ></i>
+            </div>
+          </article>
+    `);
+    $(".card-details-div").prepend(newCard);
+    if ($("input#set-default-card").is(":checked")) {
+      setDefault(newCard, ".card", ".card-holder");
+    }
+    removeModalDetails();
+  });
   // END OF CARD DETAILS
+
+  //  START OF NOTIFICATION MODAL
+  // END OF NOTIFICATION MODAL
+
+  //  START OF REVIEW MODAL
+  // END OF REVIEW MODAL
+
+  //  START OF ORDER MODAL
+  // END OF ORDER MODAL
 });
